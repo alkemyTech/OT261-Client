d0318ba6ddd63f5469002ecd5323344d2ff119de
patxi <patxiarana05@gmail.com>
2022-09-19 04:06:50 -0300

    finished back office news screen

---
+++ src/screen/backoffice-novedades/styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+
+
+export const Img = styled.img`
+width: 6rem;
+height: 6rem;
+`
--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import counterReducer from './slice/counter';
 import userReducer from './slice/user';
 import activitiesReducer from './slice/activities';
 import testimonialsReducer from './slice/testimonials';
+import newsReducer from './slice/news';
 export default configureStore({
   reducer: {
     counter: counterReducer,
     activities: activitiesReducer,
     user: userReducer,
-    testimonials:testimonialsReducer
+    testimonials:testimonialsReducer,
+    news:newsReducer,
   },
 });
 
